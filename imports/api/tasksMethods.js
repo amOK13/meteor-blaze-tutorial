fb733c6d2161c88cee3beed201cb47d036625ea8
Menoude Hountondji <menoude.hountondji@atsuke.com>
2023-02-12 22:55:40 +0100

    feat(core): Remove login packages

--- imports/api/tasksMethods.js
+++ imports/api/tasksMethods.js
@@ -6,25 +6,16 @@ Meteor.methods({
   'tasks.insert'(text) {
     check(text, String);
 
-    if (!this.userId) {
-      throw new Meteor.Error('Not authorized.');
-    }
-
     TasksCollection.insert({
       text,
       createdAt: new Date(),
-      userId: this.userId,
     });
   },
 
   'tasks.remove'(taskId) {
     check(taskId, String);
 
-    if (!this.userId) {
-      throw new Meteor.Error('Not authorized.');
-    }
-
-    const task = TasksCollection.findOne({ _id: taskId, userId: this.userId });
+    const task = TasksCollection.findOne({ _id: taskId });
 
     if (!task) {
       throw new Meteor.Error('Access denied.');
@@ -37,11 +28,7 @@ Meteor.methods({
     check(taskId, String);
     check(isChecked, Boolean);
 
-    if (!this.userId) {
-      throw new Meteor.Error('Not authorized.');
-    }
-
-    const task = TasksCollection.findOne({ _id: taskId, userId: this.userId });
+    const task = TasksCollection.findOne({ _id: taskId });
 
     if (!task) {
       throw new Meteor.Error('Access denied.');
